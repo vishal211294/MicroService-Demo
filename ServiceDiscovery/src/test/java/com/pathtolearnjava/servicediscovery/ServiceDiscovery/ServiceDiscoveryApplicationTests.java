58e479134f30f32a43e0191112218fc2af7ba732
vishal211294 <86513579+vishal211294@users.noreply.github.com>
2022-09-01 12:55:33 +0530

    Add files via upload

---
+++ ServiceDiscovery/src/test/java/com/pathtolearnjava/servicediscovery/ServiceDiscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathtolearnjava.servicediscovery.ServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
